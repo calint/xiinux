c90883772ef4c0507c5bd69713c83148eaf5f00d
calint <calin.tenitchi@gmail.com>
2015-04-10 05:29:12 -0500

    .

--- qa/test-stress.sh
+++ qa/test-stress.sh
@@ -3,22 +3,29 @@ OUT=log.txt
 SUM=log-summary.txt
 URI=
 URIS="/ /qa/q01.txt /?hello"
+NCLIENTS="1 10 100 1000"
 
-echo&&date&&
-date>$OUT&&echo -n>$SUM&&
+date>$OUT
+date>$SUM
 for URI in $URIS;do
-	echo uri: $URI>>$OUT&&
-	ab    -v2 -c1    -n1      $HOST$URI>>$OUT&&
-	ab    -v0 -c10   -n10000  $HOST$URI>>$OUT&&
-	ab    -v0 -c100  -n10000  $HOST$URI>>$OUT&&
-	ab    -v0 -c1000 -n10000  $HOST$URI>>$OUT&&
-
-	echo urik: $URI>>$OUT&&
-	ab -k -v2 -c1    -n1      $HOST$URI>>$OUT&&
-	ab -k -v0 -c10   -n10000  $HOST$URI>>$OUT&&
-	ab -k -v0 -c100  -n10000  $HOST$URI>>$OUT&&
-	ab -k -v0 -c1000 -n10000  $HOST$URI>>$OUT
-done&&
-cat $OUT|grep '^uri\|^Failed\|^Requests'>>$SUM&&
-date>>$OUT&&date>>$SUM&&
-cat $SUM&&echo
+	echo
+	echo uri: $URI>>$OUT
+#	ab    -v2 -c1    -n1      $HOST$URI>>$OUT
+	for C in $NCLIENTS;do
+		echo concurrency: $C>>$OUT
+		ab    -v0 -c$C   -n10000  $HOST$URI>>$OUT
+	done
+	
+	echo
+	echo uri keep-alive: $URI>>$OUT
+#	ab -k -v2 -c1    -n1      $HOST$URI>>$OUT
+	for C in $NCLIENTS;do
+		echo concurrency: $C>>$OUT
+		ab -k -v0 -c$C   -n10000  $HOST$URI>>$OUT
+	done
+done
+cat $OUT|grep '^uri\|^Failed\|^Requests\|^concurrency'>>$SUM
+date>>$OUT
+date>>$SUM
+cat $SUM
+echo
