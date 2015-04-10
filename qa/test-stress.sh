55eaa92c2bc6621b815aa99e80d7d1e4b581c0cc
calint <calin.tenitchi@gmail.com>
2015-04-10 05:18:49 -0500

    .

--- qa/test-stress.sh
+++ qa/test-stress.sh
@@ -2,21 +2,23 @@ HOST=localhost:8088
 OUT=log.txt
 SUM=log-summary.txt
 URI=
-URI=?hello
+URIS="/ /qa/q01.txt /?hello"
 
 echo&&date&&
 date>$OUT&&echo -n>$SUM&&
-ab    -v2 -c1    -n1      $HOST/$URI>>$OUT&&
-ab    -v0 -c10   -n10000  $HOST/$URI>>$OUT&&
-ab    -v0 -c100  -n10000  $HOST/$URI>>$OUT&&
-ab    -v0 -c1000 -n10000  $HOST/$URI>>$OUT&&
+for URI in $URIS;do
+	echo uri: $URI>>$OUT&&
+	ab    -v2 -c1    -n1      $HOST$URI>>$OUT&&
+	ab    -v0 -c10   -n10000  $HOST$URI>>$OUT&&
+	ab    -v0 -c100  -n10000  $HOST$URI>>$OUT&&
+	ab    -v0 -c1000 -n10000  $HOST$URI>>$OUT&&
 
-ab -k -v2 -c1    -n1      $HOST/$URI>>$OUT&&
-ab -k -v0 -c10   -n100000 $HOST/$URI>>$OUT&&
-ab -k -v0 -c100  -n100000 $HOST/$URI>>$OUT&&
-ab -k -v0 -c1000 -n100000 $HOST/$URI>>$OUT&&
-
-cat $OUT|grep Failed>>$SUM&&
-cat $OUT|grep Requests>>$SUM&&
+	echo urik: $URI>>$OUT&&
+	ab -k -v2 -c1    -n1      $HOST$URI>>$OUT&&
+	ab -k -v0 -c10   -n10000  $HOST$URI>>$OUT&&
+	ab -k -v0 -c100  -n10000  $HOST$URI>>$OUT&&
+	ab -k -v0 -c1000 -n10000  $HOST$URI>>$OUT
+done&&
+cat $OUT|grep '^uri\|^Failed\|^Requests'>>$SUM&&
 date>>$OUT&&date>>$SUM&&
 cat $SUM&&echo
