ddfa8620586972f560fa1f03bc45d67e72ae5e68
calint <calin.tenitchi@gmail.com>
2015-04-10 22:48:10 -0500

    .

--- qa/test-coverage.sh
+++ qa/test-coverage.sh
@@ -1,29 +1,32 @@
-HOST=localhost:8088
+HOST=localhost
+PORT=8080
+HTTP=http://localhost:8088
+
 
 echo&&date&&
 #-- - - -- -- - ------- - - - - -- - - - --- -- 
 echo " * static document"&&
-curl -si $HOST/qa/q01.txt>cmp&&
+curl -si $HTTP/qa/q01.txt>cmp&&
 diff -q cmp t01.cmp&&
 rm cmp&&
 #--- - - - - ---  - - - - -- - -- - -- - - -- -
 echo " * if-modified-since"&&
-curl -siH "if-modified-since: Fri, 10 Apr 15 07:58:44 GMT" $HOST/qa/q01.txt>cmp&&
+curl -siH "if-modified-since: Fri, 10 Apr 15 07:58:44 GMT" $HTTP/qa/q01.txt>cmp&&
 diff -q cmp t07.cmp&&
 rm cmp&&
 #--- - - - - ---  - - - - -- - -- - -- - - -- - 
 echo " * dynamic document"&&
-curl -si $HOST/?hello>cmp&&
+curl -si $HTTP/?hello>cmp&&
 diff -q cmp t02.cmp&&
 rm cmp&&
 #--- - - - - ---  - - - - -- - -- - -- - - -- - 
 echo " * resumable download"&&
-curl -sir 1- $HOST/qa/q01.txt>cmp&&
+curl -sir 1- $HTTP/qa/q01.txt>cmp&&
 diff -q cmp t05.cmp&&
 rm cmp&&
 #-- - - -- -- - ------- - - - - -- - - - --- -- 
 echo " * cached document"&&
-curl -si $HOST/>cmp&&
+curl -si $HTTP/>cmp&&
 diff -q cmp t06.cmp&&
 rm cmp&&
 #-- - - -- -- - ------- - - - - -- - - - --- -- 
@@ -33,13 +36,18 @@ diff -q cmp t08.cmp&&
 rm cmp&&
 #--- - - - - ---  - - - - -- - -- - -- - - -- - 
 echo " * page not found"&&
-curl -si $HOST/asdf.html>cmp&&
+curl -si $HTTP/asdf.html>cmp&&
 diff -q cmp t03.cmp&&
 rm cmp&&
 #--- - - - - ---  - - - - -- - -- - -- - - -- - 
 echo " * widget not found"&&
-curl -si $HOST/?asdf>cmp&&
+curl -si $HTTP/?asdf>cmp&&
 diff -q cmp t04.cmp&&
+rm cmp&&
+#--- - - - - ---  - - - - -- - -- - -- - - -- - 
+echo " * chained requests"&&
+echo $'GET / HTTP/1.1\r\n\r\nGET / HTTP/1.1\r\n\r\n'|nc localhost 8088>cmp&&
+diff -q cmp t09.cmp&&
 rm cmp&&
 #--- - - - - ---  - - - - -- - -- - -- - - -- -
 date&&echo
