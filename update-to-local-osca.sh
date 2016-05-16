a15833902ad6084b920446a67e6c25494bd63701
calin tenitchi <calin.tenitchi@gmail.com>
2016-05-16 07:30:02 +0700

    .

--- update-to-local-osca.sh
+++ update-to-local-osca.sh
@@ -2,5 +2,5 @@ FROM=$(realpath .)
 TO=$(realpath $OSCA)
 echo "from: $FROM"
 echo "  to: $TO"
-rsync -vrt --exclude "*/.*" --exclude "*/.*/" --exclude=Debug $FROM $TO
+rsync -vrt --delete --exclude "*/.*" --exclude "*/.*/" --exclude=Debug $FROM $TO
 
