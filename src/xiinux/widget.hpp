1a7b79f714bb5f51007ceaba9fbc7b244c93a663
calint <calin.tenitchi@gmail.com>
2023-09-26 14:46:48 +0200

    .

--- src/xiinux/widget.hpp
+++ src/xiinux/widget.hpp
@@ -6,10 +6,10 @@ public:
   virtual ~widget() {}
   virtual void to(reply &x) = 0;
 
-  // first call is with buf=nullptr, len=0 and the expected
-  // length of the content in total_content_len
-  // subsequent calls are the portions of the content as
-  // read from the client
+  // called when client is sending content
+  // first call is with buf=nullptr, len=0 and the length of the content in
+  // total_content_len
+  // subsequent calls are the portions of the content as read from the client
   virtual void on_content(reply &x, /*scan*/ const char *buf, const size_t len,
                           const size_t total_content_len) {}
 };
