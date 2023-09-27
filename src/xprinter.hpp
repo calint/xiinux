5f4af3197613a26a1c323aad131d0d6b4a2ba103
calint <calin.tenitchi@gmail.com>
2023-09-27 20:21:55 +0200

    .

--- src/xprinter.hpp
+++ src/xprinter.hpp
@@ -8,9 +8,9 @@ public:
   virtual xprinter &p(/*scan*/ const char *str) = 0;
   virtual xprinter &p(/*scan*/ const char *str, const size_t str_len) = 0;
   virtual xprinter &p(const int i) = 0;
-  virtual xprinter &p(const size_t i) = 0;
+  virtual xprinter &p(const size_t sz) = 0;
   virtual xprinter &p_ptr(const void *ptr) = 0;
-  virtual xprinter &p_hex(const unsigned i) = 0;
+  virtual xprinter &p_hex(const unsigned hex) = 0;
   virtual xprinter &p(const char ch) = 0;
   virtual xprinter &nl() = 0;
   virtual xprinter &html5(const char *title = "") = 0;
