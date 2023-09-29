830926d3964fba887730f29b84f43e7749e7983d
calint <calin.tenitchi@gmail.com>
2023-09-29 03:45:21 +0200

    .

--- src/web/web.hpp
+++ src/web/web.hpp
@@ -14,16 +14,20 @@ namespace xiinux::web {
 
 inline static lut<widget *(*)()> widget_path_to_factory_map{};
 
-static inline widget *(*widget_factory_for_path(const char *path))(){
+static inline widget *(*widget_factory_for_path(const char *path))() {
   return widget_path_to_factory_map[path];
 }
 
-inline static widget *widget_create_hello() { return new qa::hello(); }
-inline static widget *widget_create_typealine() { return new qa::typealine(); }
-inline static widget *widget_create_counter() { return new qa::counter(); }
-inline static widget *widget_create_chunked() { return new qa::chunked(); }
-inline static widget *widget_create_chunkedbig() { return new qa::chunkedbig(); }
-inline static widget *widget_create_chunkedbigger() { return new qa::chunkedbigger(); }
+static inline widget *widget_create_hello() { return new qa::hello(); }
+static inline widget *widget_create_typealine() { return new qa::typealine(); }
+static inline widget *widget_create_counter() { return new qa::counter(); }
+static inline widget *widget_create_chunked() { return new qa::chunked(); }
+static inline widget *widget_create_chunkedbig() {
+  return new qa::chunkedbig();
+}
+static inline widget *widget_create_chunkedbigger() {
+  return new qa::chunkedbigger();
+}
 
 static inline void widget_init_path_to_factory_map() {
   widget_path_to_factory_map.put("/qa/hello", widget_create_hello);
@@ -31,7 +35,8 @@ static inline void widget_init_path_to_factory_map() {
   widget_path_to_factory_map.put("/qa/counter", widget_create_counter);
   widget_path_to_factory_map.put("/qa/chunked", widget_create_chunked);
   widget_path_to_factory_map.put("/qa/chunkedbig", widget_create_chunkedbig);
-  widget_path_to_factory_map.put("/qa/chunkedbigger", widget_create_chunkedbigger);
+  widget_path_to_factory_map.put("/qa/chunkedbigger",
+                                 widget_create_chunkedbigger);
 }
 
 } // namespace xiinux::web
