6bf8a9f4c7aef478ed188ee53f27c10031a9c139
calint <calin.tenitchi@gmail.com>
2023-10-02 19:30:46 +0200

    .

--- make.sh
+++ make.sh
@@ -39,10 +39,10 @@ echo && ls -ho --color $BIN &&
 echo &&
 rm all.src &&
 
-#clang-tidy -header-filter=.* src/main.cpp -- -std=c++20 &&
+#./clang-tidy.sh &&
 
 #valgrind ./$BIN
 #valgrind --leak-check=full ./$BIN
-valgrind --leak-check=full --show-leak-kinds=all -s ./$BIN
+#valgrind --leak-check=full --show-leak-kinds=all -s ./$BIN
 #valgrind --leak-check=full --show-leak-kinds=all ./$BIN -bm
 echo
