e042628b84090b2ea933d6361668b6906b706cc6
calint <calin.tenitchi@gmail.com>
2023-10-02 18:50:49 +0200

    .

--- src/server.hpp
+++ src/server.hpp
@@ -60,8 +60,9 @@ public:
     server_addr.sin_addr.s_addr = INADDR_ANY;
     server_addr.sin_port = htons(uint16_t(port));
 
-    if (bind(server_fd, reinterpret_cast<struct sockaddr *>(&server_addr),
-             server_addr_size)) {
+    auto *server_sock_addr = // static cast to not trigger lint
+        static_cast<struct sockaddr *>(static_cast<void *>(&server_addr));
+    if (bind(server_fd, server_sock_addr, server_addr_size)) {
       perror("bind");
       return 4;
     }
@@ -119,9 +120,10 @@ public:
           struct sockaddr_in client_addr {};
           memset(&client_addr, 0, sizeof(client_addr));
           socklen_t client_addr_len = sizeof(client_addr);
-          const int client_fd = accept4(
-              server_fd, reinterpret_cast<struct sockaddr *>(&client_addr),
-              &client_addr_len, SOCK_NONBLOCK);
+          auto *client_sock_addr = // static cast to not trigger lint
+              static_cast<struct sockaddr *>(static_cast<void *>(&server_addr));
+          const int client_fd = accept4(server_fd, client_sock_addr,
+                                        &client_addr_len, SOCK_NONBLOCK);
           if (client_fd == -1) {
             perror("accept");
             continue;
