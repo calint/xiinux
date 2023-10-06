89804c420841774065be40b30ca927beb3308f84
calint <calin.tenitchi@gmail.com>
2023-10-06 21:48:36 +0200

    .

--- src/ui/elem.hpp
+++ src/ui/elem.hpp
@@ -18,10 +18,13 @@ public:
   [[nodiscard]] inline auto get_name() const -> const std::string & {
     return name_;
   }
+  
   [[nodiscard]] inline auto get_value() const -> const std::string & {
     return value_;
   }
+  
   inline void set_value(const std::string &value) { value_ = value; }
+
   [[nodiscard]] inline auto get_id() const -> std::string {
     std::vector<const elem *> elems{};
     const elem *el = this;
