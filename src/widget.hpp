8cbeb225550a527a1fe168fedfb2abaea6f0f8b6
calint <calin.tenitchi@gmail.com>
2025-09-16 09:14:40 +0700

    .

--- src/widget.hpp
+++ src/widget.hpp
@@ -17,10 +17,11 @@ class widget {
     virtual auto to(reply& x) -> void = 0;
 
     // called when client is sending content
-    // first call is with 'buf'=nullptr, 'buf_len'=0, 'received_len'=0 and
-    // 'content_len'=total len. subsequent calls are the portions of the content
-    // as read from the client 'received_len' keeps track of received content
-    // this far
+    // * first call is with 'buf'=nullptr, 'buf_len'=0, 'received_len'=0 and
+    // 'content_len'=total len; use to initialize state for receive
+    // * subsequent calls are the portions of the content as read from the
+    // client; 'received_len' keeps track of received content so far
+    // * at last call 'received_len==content_len'
     virtual auto on_content([[maybe_unused]] reply& x,
                             [[maybe_unused]] const char* buf, /*temporary*/
                             [[maybe_unused]] const size_t buf_len,
